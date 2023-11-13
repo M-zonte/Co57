97d79729610d9cfd796ebafdf5234c6554262434
M_zonte <matteocapitani01@gmail.com>
2023-11-13 13:35:04 +0100

    Added function to read N957 files

--- Co57/TensioneLavoroNaI.cpp
+++ Co57/TensioneLavoroNaI.cpp
@@ -8,10 +8,14 @@
 #include <iostream>
 #include "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Libs/myRootLib.cc"
 #include "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Libs/UtilityLib.cc"
+#include "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Libs/Co57Lib.cc"
+using namespace std;
+
 
 int main(int argc, char ** argv) {
     
-    
+    vector<vector<int>> data = readN957<int>("/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Data/1-TensioneLavoroNaI/1250Volt-5min.txt");
+    cout << data.at(0).size();
     
     return 0;
 }
