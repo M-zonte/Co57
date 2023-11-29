dcaa7b6f731273556a72d0d5e63e238968bad79d
M_zonte <matteocapitani01@gmail.com>
2023-11-29 16:32:09 +0100

    Perform MCA linearity analysis

--- Co57/2-LinearitaMCA.cpp
+++ Co57/2-LinearitaMCA.cpp
@@ -20,29 +20,30 @@ int main(int argc, char ** argv) {
     clock_t t_begin;
     t_begin = clock();
     
-    TString input_filename = "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Data/2-LinearitaMCA/mca_linearityGOOD.txt";
+    TString input_filename = "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Data/2-LinearitaMCA/nome esotico.txt";
     TString output_filename = "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Output/2-LinearitaMCA.root";
-    int maxVoltage = 5;
+    TString voltage_filename = "/Users/matteocapitani/Desktop/Università/4 Anno/Laboratorio di Misure Nucleari/Co57/Data/2-LinearitaMCA/voltage.txt";
     
     vector<vector<double>> data = readN957<double>(input_filename);
+    vector<pair<double, double>> voltage = readPAIR<double>(voltage_filename);
     vector<double> media;
     vector<double> sigma;
     
     TH1D *MCA_Linearity = new TH1D("MCA Linearity", "MCA Linearity", data.at(0).size(), 0, 8192);
+
     for (int i=0; i< data.at(0).size(); i++) {
         MCA_Linearity->SetBinContent(i, data.at(2).at(i));
     }
     
-    int peakFound = MCA_Linearity->ShowPeaks();
+    int peakFound = MCA_Linearity->ShowPeaks(1, "noMarkov", 0.6);
     TList *functions = MCA_Linearity->GetListOfFunctions();
     TPolyMarker *pm = (TPolyMarker*)functions->FindObject("TPolyMarker");
     
     TFile *f = new TFile(output_filename, "RECREATE");
     f->cd();
     
-    for (int i=1; i<=maxVoltage; i++) {
-        int peakPosition = 760*i;
-        TF1 *fit = new TF1("Peak", "gaus(0)", pm->GetX()[i-1]-10, pm->GetX()[i-1]+10);
+    for (int i=0; i<peakFound; i++) {
+        TF1 *fit = new TF1("Peak", "gaus(0)", pm->GetX()[i]-15, pm->GetX()[i]+15);
         TFitResultPtr *fitPtr = new TFitResultPtr (MCA_Linearity->Fit(fit, "SRLQ+"));
         media.push_back(fit->GetParameter(1));
         sigma.push_back(fit->GetParameter(2));
@@ -52,16 +53,29 @@ int main(int argc, char ** argv) {
 
     sort(media.begin(), media.end());
     sort(sigma.begin(), sigma.end());
+    
+    TApplication theApp("theApp", &argc, argv);
+    TCanvas *c = new TCanvas();
+    c->cd();
     TGraphErrors *MCA_analysis = new TGraphErrors();
-    for (int i=0; i<maxVoltage; i++) {
-        MCA_analysis->SetPoint(i, i+1, media.at(i));
-        MCA_analysis->SetPointError(0.03*i, sigma.at(i));
+    MCA_analysis->TNamed::SetNameTitle("MCA Analysis", "MCA Linearity Analysis");
+    MCA_analysis->GetXaxis()->SetTitle("Volt (V)");
+    MCA_analysis->GetYaxis()->SetTitle("Channel");
+    
+    for (int i=0; i<voltage.size(); i++) {
+        MCA_analysis->SetPoint(i, voltage.at(i).first, media.at(i));
+        MCA_analysis->SetPointError(i, voltage.at(i).second/32, sigma.at(i));
     }
     MCA_analysis->SetMarkerStyle(20);
-    TF1 *fit = new TF1("Linearity", "pol1(0)", 0, 8);
+    MCA_analysis->SetMarkerSize(0.7);
+    TF1 *fit = new TF1("Linearity", "pol1(0)", 1.5, 9.9);
+    gStyle->SetOptFit(1111);
     TFitResultPtr *fitPtr = new TFitResultPtr (MCA_analysis->Fit(fit, "SR+"));
+    MCA_analysis->Draw("AP");
+    theApp.Run();
     
     MCA_analysis->Write();
+    
     f->Close();
     
     t_begin = clock() - t_begin;
